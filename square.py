abc585e197ead895b124190423b9c6abbe7d83c2
Mayur Muralidhar <11.mayur@gmail.com>
2017-07-11 12:52:47 +0400

    Added functions to call shapes.

--- square.py
+++ square.py
@@ -3,16 +3,21 @@ import turtle
 window = turtle.Screen()
 window.bgcolor("blue")
 
-pen = turtle.Turtle()
-pen.shape("triangle")
-pen.color("red")
-pen.speed(1)
+def draw_square():
+    pen = turtle.Turtle()
+    pen.shape("triangle")
+    pen.color("red")
+    pen.speed(1)
 
-for i in range(4):
-    pen.forward(100)
-    pen.right(90)
+    for i in range(4):
+        pen.forward(100)
+        pen.right(90)
 
-pen2 = turtle.Turtle()
-pen2.circle(100)
+def draw_circle():
+    pen2 = turtle.Turtle()
+    pen2.circle(100)
+
+draw_square()
+draw_circle()
 
 window.exitonclick()
