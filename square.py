cdba0cdffff1494cb3bfcc96f8184e559e3f29b5
Mayur Muralidhar <11.mayur@gmail.com>
2017-07-11 13:00:19 +0400

    Three shapes

--- square.py
+++ square.py
@@ -17,7 +17,16 @@ def draw_circle():
     pen2 = turtle.Turtle()
     pen2.circle(100)
 
+def draw_triangle():
+    pen3 = turtle.Turtle()
+    pen3.color("white")
+    #pen3.triangle(100)
+    for i in range(3):
+        pen3.forward(100)
+        pen3.left(120)
+
 draw_square()
 draw_circle()
+draw_triangle()
 
 window.exitonclick()
