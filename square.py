437360da24e6cb7995a0ea8c4194722540e76f79
Mayur Muralidhar <11.mayur@gmail.com>
2017-07-11 12:38:02 +0400

    Changed color parameters.

--- square.py
+++ square.py
@@ -1,8 +1,13 @@
 import turtle
 
 window = turtle.Screen()
+window.bgcolor("blue")
 
 pen = turtle.Turtle()
+pen.shape("triangle")
+pen.color("red")
+pen.speed(1)
+
 for i in range(4):
     pen.forward(100)
     pen.right(90)
